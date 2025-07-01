d3ef8486d0621397c1fa50263a13a4ae2111cd58
Shaileshprajapati18 <shaileshprajapati182005@gmail.com>
2025-07-01 14:37:43 +0530

    Updated code for Tummy Tales project

--- app/src/main/java/com/example/demo/Activities/AskAmmaActivity.java
+++ app/src/main/java/com/example/demo/Activities/AskAmmaActivity.java
@@ -31,7 +31,6 @@ public class AskAmmaActivity extends AppCompatActivity {
                 startActivity(new Intent(AskAmmaActivity.this, MomNetworkActivity.class));
             }
         });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
